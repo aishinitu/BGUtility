128bdc6831663901fcdbf98e15fa0f66328dedb0
Aishika Pradhan <aishinitu@gmail.com>
2023-04-03 15:39:05 +0530

    Initial commit

--- Tests/BGUtilityTests/Array+ExtensionsTests.swift
+++ Tests/BGUtilityTests/Array+ExtensionsTests.swift
@@ -12,14 +12,14 @@ class Array_ExtensionsTests: XCTestCase {
 
 
     func testProperValue() throws {
-        let numbers = [1,2,3,4,5]
+        let numbers = [1,2,3,4,5,7]
         let num1  = numbers[safeIndex: 0]
         XCTAssert(num1 == 1)
     }
 
     func testImProperValue() throws {
         let numbers = [1,2,3,4,5]
-        let num1  = numbers[safeIndex: 5]
+        let num1  = numbers[safeIndex: 10]
         XCTAssert(num1 == nil)
     }
 
