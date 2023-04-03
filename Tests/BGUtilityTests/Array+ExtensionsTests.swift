a87d8ba6cd122c143ced61aab3019f6ea2edf975
Aishika Pradhan <aishinitu@gmail.com>
2023-04-03 16:20:21 +0530

    second commit

--- Tests/BGUtilityTests/Array+ExtensionsTests.swift
+++ Tests/BGUtilityTests/Array+ExtensionsTests.swift
@@ -12,7 +12,7 @@ class Array_ExtensionsTests: XCTestCase {
 
 
     func testProperValue() throws {
-        let numbers = [1,2,3,4,5,7]
+        let numbers = [1,2,3,8,5,7]
         let num1  = numbers[safeIndex: 0]
         XCTAssert(num1 == 1)
     }
